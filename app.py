d89001ff131ed50dee3bb19c07162ce460a3e468
GabryelKadmo <gabryelkadmo@gmail.com>
2024-05-18 20:03:39 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from transformers import BertTokenizer, BertForSequenceClassification
+from torch.nn.functional import softmax
+import torch
+
+# Carregar o modelo pré-treinado e o tokenizer
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name)
+
+# Definindo a função para classificar o sentimento
+def classify_sentiment(text):
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    probs = softmax(outputs.logits, dim=1)
+    sentiment_index = torch.argmax(probs, dim=1).item()
+    sentiment_label = 'Neutro' if sentiment_index == 2 else 'Positivo' if sentiment_index > 2 else 'Negativo'
+    return sentiment_label
+
+# Criando a interface Streamlit
+st.title('Classificador de Sentimentos')
+user_input = st.text_area("Digite uma frase para analisar o sentimento:", value="", height=150)
+if st.button('Classificar Sentimento'):
+    if user_input.strip():
+        sentiment = classify_sentiment(user_input)
+        st.write(f"O sentimento desta frase é: **{sentiment}**")
+    else:
+        st.write("Por favor, digite uma frase para analisar.")
