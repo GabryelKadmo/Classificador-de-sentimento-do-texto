405ae36504282b05dd62d1a8c926de13d428856f
GabryelKadmo <gabryelkadmo@gmail.com>
2024-05-18 23:16:48 -0300

    fix: i dont know

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from torch.nn.functional import softmax
 import torch
 from transformers import AutoConfig
 
-# Load model and tokenizer outside of the Streamlit app's main execution loop
 config = AutoConfig.from_pretrained("bert-base-cased")
+
+# Carregar o modelo pré-treinado e o tokenizer
+st.set_page_config(page_title="Classificador de Sentimentos", page_icon=":speech_balloon:", layout="centered")
 model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
 tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertForSequenceClassification.from_pretrained(model_name)
 
-# Function to classify sentiment
+# Definindo a função para classificar o sentimento
 def classify_sentiment(text):
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
     with torch.no_grad():
@@ -20,19 +22,9 @@ def classify_sentiment(text):
     sentiment_label = 'Neutro' if sentiment_index == 2 else 'Positivo' if sentiment_index > 2 else 'Negativo'
     return sentiment_label
 
-# Streamlit interface
-st.set_page_config(page_title="Classificador de Sentimentos", page_icon=":speech_balloon:", layout="centered")
+# Criando a interface Streamlit
 st.title('Classificador de Sentimentos')
-
-# Main input form
 user_input = st.text_area("Digite uma frase para analisar o sentimento:", value="", height=150)
-
-# Store the button instance in a variable
-classify_button = st.button('Classificar Sentimento')
-
-# Perform sentiment analysis when button is clicked
-if classify_button and user_input.strip():
+if st.button('Classificar Sentimento'):
     sentiment = classify_sentiment(user_input)
     st.write(f"O sentimento desta frase é: **{sentiment}**")
-elif classify_button and not user_input.strip():
-    st.warning("Por favor, digite uma frase para análise de sentimento.")
