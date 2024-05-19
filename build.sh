8527dacef6aac8b002e5576418ef6b13522525e2
GabryelKadmo <gabryelkadmo@gmail.com>
2024-05-18 21:29:36 -0300

    fix: fixing build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
-pip install --no-cache-dir -r requirements.txt
+#!/bin/bash
 
-streamlit run app.py
+# Define o caminho para o executável Python
+PYTHON_EXEC=$(which python)
+
+# Instala o Streamlit (se ainda não estiver instalado)
+$PYTHON_EXEC -m pip install streamlit
+
+# Instala as dependências Python
+$PYTHON_EXEC -m pip install --no-cache-dir -r requirements.txt
+
+# Inicia a aplicação Streamlit
+$PYTHON_EXEC -m streamlit run app.py
